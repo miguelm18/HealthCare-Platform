328440c9e427bb36955bde4910f83cce132f0f51
Miguel Molina <miguelam0318@gmail.com>
2024-04-09 18:47:49 -0700

    Testing for repositories

--- src/main/java/com/examplehealthcare/Repository/DoctorRepository.java
+++ src/main/java/com/examplehealthcare/Repository/DoctorRepository.java
@@ -1,9 +1,10 @@
 package com.examplehealthcare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.examplehealthcare.model.Doctor;
 
+@Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     
 }
---
+++ src/test/java/com/examplehealthcare/repository/DoctorRepositoryTest.java
@@ -0,0 +1,5 @@
+package com.examplehealthcare.repository;
+
+public class DoctorRepositoryTest {
+    
+}
