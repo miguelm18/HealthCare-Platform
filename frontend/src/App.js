dad648e3472922f6892d3fc7820d93d38031fb92
BrendanMWong <tobrendanw@gmail.com>
2024-04-22 18:43:31 -0700

    added mia code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Layout/Header';
-import Sidebar from './Layout/Sidebar';
-import Home from './Pages/Dashboard';
-import About from './Pages/Patients';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LoginSignUp from './LoginSignUp';
+import MainDashboard from './MainDashboard';
+//import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Sidebar />
-        <div className="content">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginSignUp />} />
+        <Route path="/dashboard" element={<MainDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
---
+++ frontend/src/MainDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainDashboard() {
+  return (
+    <div>
+      <h1>MainDashboard</h1>
+      {/* Add your dashboard content here */}
+    </div>
+  );
+}
+
+export default MainDashboard;
