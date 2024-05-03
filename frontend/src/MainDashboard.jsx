e55aff76aa2a3a5d674a468b7eb43f2605a998fe
BrendanMWong <tobrendanw@gmail.com>
2024-05-02 20:05:58 -0700

    a few edits to searchbar functionality

--- frontend/src/MainDashboard.jsx
+++ frontend/src/MainDashboard.jsx
@@ -3,13 +3,19 @@ import Header from './Layout/Header';
 import Sidebar from './Layout/Sidebar';
 import './MainDashboard.css'; // Import the CSS file for MainDashboard
 import SearchingBar from './SearchingBar';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function MainDashboard() {
 
   const [patientName, setPatientName] = useState("blank");
+  const [filteredPatients, setFilteredPatients] = useState([]);
 
   const parentName = (value) => {
     setPatientName(value);
+  };
+
+  const parentTable = (value2) => {
+    setFilteredPatients(value2);
   };
 
   return (
@@ -20,11 +26,35 @@ function MainDashboard() {
       <h1>MainDashboard</h1>
       <p>This is the content of the MainDashboard.</p>
       <div className="search-bar-container">
-        <SearchingBar parentName={parentName} />
+        <SearchingBar parentName={parentName} parentTable={parentTable} />
       </div>
       <p>implement notifications somewhere on this page</p>
       <p>implement the calendar thingy somewhere on this page</p>
       <p>Patient Name Searched: {patientName}</p>
+      <br></br>
+      <br></br>
+      {filteredPatients.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Gender</th>
+              <th>Condition</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredPatients.map((patient, index) => (
+              <tr key={index}>
+                <td><Link to="/patient1">{patient.name}</Link></td>
+                <td>{patient.age}</td>
+                <td>{patient.gender}</td>
+                <td>{patient.condition}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
--- frontend/src/SearchingBar.jsx
+++ frontend/src/SearchingBar.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 //import './SearchingBar.css';
+import patients from './MockData'; // Import the patient data
 
-function SearchingBar({ parentName }) {
+function SearchingBar({ parentName, parentTable }) {
   const [storedName, setStoredName] = useState("");
 
   const handleInputChange = (event) => {
     const inputName = event.target.value;
     setStoredName(inputName); // Update storedName with the new value
     parentName(inputName); // Pass the new value directly to parentName
+    //parentTable(["hi"]);
+    if (inputName === '') {
+      parentTable([]);
+    } else {
+      parentTable(
+        patients.filter((patient) =>
+          patient.name.toLowerCase().startsWith(inputName.toLowerCase())
+        )
+      );
+    }
   };
 
   return (
